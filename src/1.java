f9ec8594b875a974712701d83db9bd831b331568
veyerlinda <veyerlinda@gmail.com>
2025-03-05 19:56:49 +0000

    Add 1.java

---
+++ src/1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println(num);
+    }
+}
