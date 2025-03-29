f5e84a136169106814b33503a6424e673f201306
veyerlinda <veyerlinda@gmail.com>
2025-03-29 19:45:31 +0000

    Add 21.java

---
+++ src/21.java
@@ -0,0 +1,10 @@
+public class SimpleSchoolProject {
+    public static void main(String[] args) {
+        // Example of adding elements to an ArrayList in Java
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        System.out.println(numbers);  // [1, 2, 3]
+    }
+}
