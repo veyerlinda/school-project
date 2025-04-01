8ccf38eacfff583125c0a0c093bb92e665b53949
veyerlinda <veyerlinda@gmail.com>
2025-04-01 22:38:46 +0000

    Add 24.java

---
+++ src/24.java
@@ -0,0 +1,10 @@
+public class SimpleSchoolProject {
+    public static void main(String[] args) {
+        // Example of student information
+        String name = "John";
+        int age = 16;
+        
+        // Main method logic
+        System.out.println("Hello, my name is " + name + " and I am " + age + " years old.");
+    }
+}
