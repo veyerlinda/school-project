3fa32cf465e4950991647e35e7669cbe9f142268
veyerlinda <veyerlinda@gmail.com>
2025-04-11 11:13:04 +0000

    Add 32.java

---
+++ src/32.java
@@ -0,0 +1,10 @@
+public class SimpleSchoolProject {
+    public static void main(String[] args) {
+        // Initialize variables
+        int age = 18;
+        String name = "John Doe";
+
+        // Print greeting message
+        System.out.println("Hello, " + name + "! You are " + age + " years old.");
+    }
+}
