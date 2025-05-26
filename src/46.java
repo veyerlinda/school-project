f3092e10a97ea352a5699e44bc57b1b0ea9dac49
veyerlinda <veyerlinda@gmail.com>
2025-05-26 03:15:38 +0000

    Add 46.java

---
+++ src/46.java
@@ -0,0 +1,6 @@
+public class SimpleSchoolProject {
+    public static void main(String[] args) {
+        // Code for your simple school project goes here
+        System.out.println("Hello, World!");
+    }
+}
